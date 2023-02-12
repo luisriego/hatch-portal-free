7de000ce01b13dd3df1306fa1d48c237306c2167
luisriego <luisriego@hotmail.com>
2023-02-12 19:44:49 -0300

    without repo

--- src/Service/HomepageService.php
+++ src/Service/HomepageService.php
@@ -15,28 +15,11 @@ class HomepageService
 
     public function handle(): array
     {
-        if (null === $result = $this->topicRepository->findBy(['toPublish' => true], ['type' => 'ASC'], 3 )) {
-            throw new NotFoundHttpException();
-        }
-
-        return $result;
-//        return [];
-//        return [
-//            [
-//                'title' => 'Inovar na transição energética',
-//                'subtitle' => 'Defendendo um futuro de baixo carbono.',
-//                'case_study' => 'Eletrolizadores de membrana de intercambio de prótons (PEM) 20MW'
-//            ],
-//            [
-//                'title' => 'Inovar nas soluções urbanas',
-//                'subtitle' => 'Cidades mais inteligentes não são só ideais sociais, são um imperativo econômico',
-//                'case_study' => 'O impacto socioeconômico do estudo Gautrain'
-//            ],
-//            [
-//                'title' => 'Inovação na mineração',
-//                'subtitle' => 'Abordagens para uma nova era da mineração',
-//                'case_study' => 'Planta piloto produz 40% a mais de alumínio'
-//            ]
-//        ];
+//        if (null === $result = $this->topicRepository->findBy(['toPublish' => true], ['type' => 'ASC'], 3 )) {
+//            throw new NotFoundHttpException();
+//        }
+//
+//        return $result;
+        return [];
     }
 }
