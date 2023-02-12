a342777c40b317f180abbac32016794484ee895f
luisriego <luisriego@hotmail.com>
2023-02-12 20:00:34 -0300

    test db

--- src/Controller/HomepageController.php
+++ src/Controller/HomepageController.php
@@ -2,15 +2,19 @@
 
 namespace App\Controller;
 
+use App\Repository\UserRepository;
 use App\Service\HomepageService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Security;
 
 class HomepageController extends AbstractController
 {
     public function __construct(
-        private readonly HomepageService $homepageService
+        private readonly HomepageService $homepageService,
+        private readonly Security $security,
+        private readonly UserRepository $userRepo,
     ) {
     }
 
@@ -19,10 +23,19 @@ class HomepageController extends AbstractController
     public function __invoke(): Response
     {
         $randomData = $this->homepageService->handle();
+        $user = $this->security->getUser();
+        $noUser = null;
+        if (!$user) {
+            $users = $this->userRepo->findAll();
+            $user = null;
+            $noUser = $users[0]->getEmail();
+        }
 
         return $this->render('homepage/index.html.twig',
             [
                 'data' => $randomData,
+                'user' => $user,
+                'noUser' => $noUser,
             ]);
     }
 }
