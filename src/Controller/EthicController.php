6391f566ce6edbfbd19c7352efa9061711fb849e
luisriego <luisriego@hotmail.com>
2023-03-26 10:46:08 -0300

    about-us changes and podcasts page

---
+++ src/Controller/EthicController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use App\Service\AboutUsService;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class EthicController extends AbstractController
+{
+    public function __construct(
+    ) {
+    }
+
+    #[Route('/ethic', name: 'app_ethic')]
+    public function __invoke(): Response
+    {
+        return $this->render('about/ethic.html.twig',
+            [
+                'breadcrumb' => 'About the Company',
+            ]);
+    }
+}
---
+++ src/Controller/PodcastsController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use App\Service\AboutUsService;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class PodcastsController extends AbstractController
+{
+    public function __construct(
+    ) {
+    }
+
+    #[Route('/podcasts', name: 'app_podcasts')]
+    public function __invoke(): Response
+    {
+        return $this->render('podcasts/podcasts.html.twig',
+            [
+                'breadcrumb' => 'Podcasts',
+            ]);
+    }
+}
