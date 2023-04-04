535615b9212954fedb76a64e3a6d1e9cbaad4faa
luisriego <luisriego@hotmail.com>
2023-04-04 15:14:27 -0300

    ckeditor installed and working

--- config/bundles.php
+++ config/bundles.php
@@ -15,4 +15,5 @@ return [
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
+    FOS\CKEditorBundle\FOSCKEditorBundle::class => ['all' => true],
 ];
--- src/Form/FactFormType.php
+++ src/Form/FactFormType.php
@@ -3,6 +3,7 @@
 namespace App\Form;
 
 use App\Entity\Fact;
+use FOS\CKEditorBundle\Form\Type\CKEditorType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -13,7 +14,7 @@ class FactFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
+            ->add('name', CKEditorType::class)
 //            ->add('previous', SubmitType::class)
             ->add('next', SubmitType::class)
             ->add('addNew', SubmitType::class)
