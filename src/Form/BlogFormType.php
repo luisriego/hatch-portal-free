95164a697d2d71c38a63637c5847a3f246b2676e
luisriego <luisriego@hotmail.com>
2023-05-07 21:27:25 -0300

    add constraint on blog duplication

--- src/Form/BlogFormType.php
+++ src/Form/BlogFormType.php
@@ -4,9 +4,11 @@ namespace App\Form;
 
 use App\Entity\Blog;
 use FOS\CKEditorBundle\Form\Type\CKEditorType;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\Image;
@@ -34,6 +36,13 @@ class BlogFormType extends AbstractType
     {
         $resolver->setDefaults([
             'data_class' => Blog::class,
+            'constraints' => [
+                new UniqueEntity([
+                    'entityClass' => Blog::class,
+                    'fields' => 'title',
+                    'message' => 'O título deve ser único, parece que já há um artigo com esse mesmo título'
+                ]),
+            ],
         ]);
     }
 }
