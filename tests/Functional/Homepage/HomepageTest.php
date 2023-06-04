afd90187b895965bac8f6b9c24ed4d55d39c120d
luisriego <luisriego@hotmail.com>
2023-06-04 08:58:03 -0300

    new nav and footer navigation

--- tests/Functional/Homepage/HomepageTest.php
+++ tests/Functional/Homepage/HomepageTest.php
@@ -1,12 +1,12 @@
 <?php
 
-namespace App\Tests;
+namespace App\Functional\Homepage\Tests;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class HomepageTest extends WebTestCase
 {
-    public function testSomething(): void
+    public function testHomepageBasics(): void
     {
         $client = static::createClient();
         $crawler = $client->request('GET', '/');
@@ -16,5 +16,14 @@ class HomepageTest extends WebTestCase
         $this->assertSelectorTextContains('h2', 'Últimos Posts do nosso Blog');
         $this->assertSelectorTextContains('h3', 'As inscrições se encerram em 31 de julho de 2023.');
         $this->assertSelectorTextContains('a h3', 'Post 1');
+    }
+
+    public function testHomepageMenu(): void
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/');
+
+        $this->assertResponseIsSuccessful();
+        $this->assertSelectorTextContains('.nav a', 'Projetos');
     }
 }
